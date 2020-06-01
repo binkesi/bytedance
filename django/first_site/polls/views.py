2a5ace3c4122924408fd7bd21c8c8382c286f67e
Guannan GN2 Sun <sungn2@Lenovo.com>
2020-06-01 17:26:55 +0800

    ommit future question

--- django/first_site/polls/views.py
+++ django/first_site/polls/views.py
@@ -6,6 +6,7 @@ from django.http import Http404
 from django.urls import reverse
 from django.db.models import F
 from django.views import generic
+from django.utils import timezone
 
 # Create your views here.
 class IndexView(generic.ListView):
@@ -13,7 +14,7 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
     
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
         
 class DetailView(generic.DetailView):
     model = Question
